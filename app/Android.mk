437a78851e5254099277647f73deb37ef61896c6
zhaogf1 <zhaogf1@lenovo.com>
2017-07-19 16:46:58 +0800

    for playstore release stategy , remove feature:"com.motorola.photoeditor" ,only remain:"com.motorola.hardware.mods"
    Change-Id: Ia34e9214db548607cb3d239a7b25db14ff94bdac
    Reviewed-on: https://gerrit.mot.com/1028585
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Zhiyong Gu <guzy2@lenovo.com>
    Submit-Approved: Jira Key

--- app/Android.mk
+++ app/Android.mk
@@ -13,25 +13,10 @@ LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
 LOCAL_CERTIFICATE := common
 
-LOCAL_REQUIRED_MODULES := \
-    com.motorola.livestream.xml
-
 #LOCAL_SRC_FILES := $(LOCAL_PATH)/build/outputs/apk/MotoLiveStream-release.apk
 LOCAL_GRADLE_TASKS := -PCLEAN_MOTOLIB -PRELEASE_VERSION assembleRelease
 LOCAL_SRC_FILES := $(LOCAL_PATH)/build/outputs/apk/$(LOCAL_MODULE)-release.apk
 
 include $(BUILD_GRADLE_PACKAGE)
 
-# ========================================================
-# Install the permissions file into system/etc/permissions
-# to ensure Market Upgrades can only occur to devices which
-# have preloaded this application
-# ========================================================
-include $(CLEAR_VARS)
-LOCAL_MODULE := com.motorola.livestream.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
 
